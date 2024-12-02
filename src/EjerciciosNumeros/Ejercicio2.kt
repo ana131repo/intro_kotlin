c54643eeacce3c83946af5c0ed58a35e3141439c
ana131 <antakarana131@gmail.com>
2024-12-02 11:22:31 +0100

    Ejercicio2

---
+++ src/EjerciciosNumeros/Ejercicio2.kt
@@ -0,0 +1,15 @@
+package EjerciciosNumeros
+
+fun main(){
+    /*Ejercicio 2
+Crea una función que dado 2 números enteros los multiplique y devuelva el resultado
+Ejemplo:2*2 -> 4
+*/
+    var num1: Int= 5
+    var num2: Int= 8
+
+    var multiplicar: Int = (num1*num2)
+
+    println("La multiplicación de dos números enteros $num1 x $num2 = $multiplicar")
+
+}

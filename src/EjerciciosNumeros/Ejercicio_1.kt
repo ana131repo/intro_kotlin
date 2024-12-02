988c1ae35aa54c4bd90d967b2381d439b5b665b6
ana131 <antakarana131@gmail.com>
2024-12-02 11:15:35 +0100

    Ejercicio1

--- src/EjerciciosNumeros/Ejercicio_1.kt
+++ src/EjerciciosNumeros/Ejercicio_1.kt
@@ -1,2 +1,14 @@
 package EjerciciosNumeros
 
+fun main(){
+/*Ejercicio 1
+Crea una función que dado 2 números enteros los sume y devuelva el resultado
+Ejemplo: 2+2 -> 4
+ */
+ var num1: Int = 2
+ var num2: Int= 4
+
+    println("Suma de dos números enteros " + num1 + " + " + num2 +" = "+(num1+num2))
+
+
+}

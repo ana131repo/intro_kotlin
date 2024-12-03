4f9c6a9ab11efbe6b1091c4fed8c46507f458a18
ana131 <antakarana131@gmail.com>
2024-12-03 11:31:21 +0100

    fix - exercicio sintax

--- src/EjerciciosNumeros/Ejercicio7.kt
+++ src/EjerciciosNumeros/Ejercicio7.kt
@@ -6,10 +6,15 @@ Calcula el volumen de una piscina dado el ancho, largo y la altura
 Ejemplo:
 ancho 4 largo 4 alto 3 -> 48
     */
-    val ancho: Int =4
-    val largo: Int =4
-    val alto: Int =3
-    val resultado = ancho*largo*alto
+    print("Calcular el volumen de una pìscina")
+    println("Escribe el ancho")
+    val ancho = readln()
+    println("Escribe el largo")
+    val largo = readln()
+    println("Escribe la altura")
+    val alto = readln()
+    /*val resultado = ancho*largo*alto
 
-     println("Volumen = (ancho $ancho * largo $largo * alto $alto) --> $resultado")
+     println("Volumen = (ancho $ancho * largo $largo * alto $alto) --> $resultado")*/
 }
+fun volumen_piscina(){}

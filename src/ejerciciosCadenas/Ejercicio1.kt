77f492c2ae7696c98bbc8d7e94b19de22f7c21d2
ana131 <antakarana131@gmail.com>
2024-12-03 12:44:42 +0100

    update exercice 1 strings

---
+++ src/ejerciciosCadenas/Ejercicio1.kt
@@ -0,0 +1,19 @@
+package ejerciciosCadenas
+
+fun main(){
+   /* Crea una función que dados 2 strings los concatene y los devuelva
+    Ejemplo:
+    "esta es una cadena" y "otra cadena" -> "esta es una cadenaotra cadena"*/
+
+    println("Escribe un texto")
+    var cad1 = readln()
+    println("Escribe otro texto")
+    var cad2 = readln()
+    concatenar(cad1,cad2)
+
+}
+
+fun concatenar(cad1:String,cad2:String){
+   val cadenaCompleta = cad1 + cad2
+    println(cadenaCompleta)
+}

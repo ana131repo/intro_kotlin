9dcf2e724a27068f7a2c4c01f716dbe212094f8a
ana131 <antakarana131@gmail.com>
2024-12-03 10:01:56 +0100

    Fix - exercise-2 sintax

--- src/EjerciciosNumeros/Ejercicio2.kt
+++ src/EjerciciosNumeros/Ejercicio2.kt
@@ -1,15 +1,28 @@
 package EjerciciosNumeros
 
-fun main(){
+fun main() {
     /*Ejercicio 2
 Crea una función que dado 2 números enteros los multiplique y devuelva el resultado
 Ejemplo:2*2 -> 4
 */
-    var num1: Int= 5
-    var num2: Int= 8
 
-    var multiplicar: Int = (num1*num2)
 
+    print("Dime número 1")
+    val num1 = readln().toIntOrNull()
+    print("Dime número 2")
+    val num2 = readln().toIntOrNull()
+    if (num1 != null && num2 != null) {
+        multiplicar(num1, num2)
+    } else {
+        println("Error: Los números que ingresaste son invalidos")
+    }
+
+
+}
+
+fun multiplicar(num1: Int, num2: Int) {
+
+    var multiplicar = num1 * num2
     println("La multiplicación de dos números enteros $num1 x $num2 = $multiplicar")
 
 }

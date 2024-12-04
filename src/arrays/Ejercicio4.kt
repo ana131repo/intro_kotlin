ef8fb60bf82a83d67abe90d485204bf78c0fa49d
ana131 <antakarana131@gmail.com>
2024-12-04 14:13:41 +0100

    update exercise4

---
+++ src/arrays/Ejercicio4.kt
@@ -0,0 +1,10 @@
+package arrays
+
+fun main(){
+    /*Ejercicio 4
+    Crea una función que dado un conjunto de enteros los ordene de mayor a menor y devuelva el array resultante. NO SE PUEDE USAR SORT
+
+            Ejemplo:
+    [6,2,3,4,5,1] -> [1,2,3,4,5,6]*/
+    
+}

bee80142ab0eb2dd7fedfa7e9fc1b6dd4033b1c7
ana131 <antakarana131@gmail.com>
2024-12-04 10:31:24 +0100

    fix - update

--- src/ejerciciosCadenas/Ejercicio3.kt
+++ src/ejerciciosCadenas/Ejercicio3.kt
@@ -8,24 +8,31 @@ fun main() {
      "esta frase es de ejemplo" caracter "o" -> "esta frase es de ejempl"*/
 
     println("Escribe una texto")
-    var texto = readln()
+    val texto = readln()
 
     println("Escribe el caracter que quieres eliminar")
-    var eliminar = readln()[0]
-    eliminarCaracter(texto, eliminar)
+    val eliminar = readln()[0]
+    val resultado =  eliminarCaracter(texto, eliminar)
+    println(resultado)
 
 
 }
 
 fun eliminarCaracter(texto: String, eliminar: Char): String {
-    if (texto.startsWith(eliminar)) {
-        println(texto.substring(1))
 
-    }
-    if(texto.endsWith(eliminar)) {
-        println(texto.substring(0, texto.length - 1))
+    var salida = texto.trim()
+    if(salida.isEmpty()){
+        return " Este texto esta vacio"
     }
 
-    return texto
+    if (salida.startsWith(eliminar)) {
+        salida = salida.substring(1)
+    }
 
+    if (salida.endsWith(eliminar)) {
+        salida = salida.substring(0, salida.length - 1)
+
+    }
+
+    return salida
 }

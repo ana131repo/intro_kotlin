8bed349eaa4ee743c17de17fba4edfe4e4d8c2fd
ana131 <antakarana131@gmail.com>
2024-12-04 12:35:55 +0100

    update Exercise 2

---
+++ src/arrays/Ejercicio2.kt
@@ -0,0 +1,39 @@
+package arrays
+
+fun main() {
+    /*Ejercicio 2
+    Crea una función que dado un conjunto de enteros devuelva si existe un número dentro de él
+
+    Ejemplo:
+    [1,2,3,4,5,6] existe 3 -> true*/
+
+    do {
+        menu()
+        println("Selecciona")
+        val opcion = readln().toIntOrNull()
+
+
+        var conjunto = mutableListOf(1, 2, 3, 4, 5)
+        println("Escribe un número")
+        val numero = readln().toIntOrNull()
+
+        if (numero != null) {
+            println(isEsxiste(numero, conjunto))
+        } else {
+            println("ERROR: dato no valido")
+        }
+
+
+    } while (opcion != 0)
+
+}//fin main
+
+fun isEsxiste(numero: Int, conjunto: MutableList<Int>): Boolean {
+    val existe = numero in conjunto
+    return existe
+}
+
+fun menu() {
+    println("Jugar [1]")
+    println("Terminar [0]")
+}
